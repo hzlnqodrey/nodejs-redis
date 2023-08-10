ac45253e3f3b6c780939d3920d7c1bc8d6375d39
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-08-11 01:38:20 +0700

    successfully fix the Redis NodeJs server error,client is closed, now redis is running seamesly

--- server.js
+++ server.js
@@ -4,34 +4,63 @@ const axios = require('axios')
 
 // REDIS CONFIG
 const Redis = require('redis')
-const client = Redis.createClient({
+const redisClient = Redis.createClient()
+redisClient.connect().catch(console.error)
     // if url production: redis://redis-server:6379
     // if not just leave it as is
-})
+const DEFAULT_EXPIRATION = 3600 // 1 hour
 
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 app.get('/',  async (req, res) => {
-    res.send("Hello Redis NodeJS")
+    res.send("Hello Redis NodeJS AAAAB")
 })
 
 app.get('/photos', async (req, res) => {
+    const albumId = req.query.albumId;
     
-    const albumId = req.query.albumId
-    const { data } = await axios.get(
-        `https://jsonplaceholder.typicode.com/photos`,
-        { params: { albumId } }    
-    )
+    try {
+        const { data } = await axios.get(
+            `https://jsonplaceholder.typicode.com/photos`,
+            { params: { albumId } }
+        );
 
-    res.json(data)
+        try {
+            await redisClient.setEx('photos', DEFAULT_EXPIRATION, JSON.stringify(data));
+            res.json(data);
+        } catch (redisError) {
+            console.error('Error in Redis setEx:', redisError);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    } catch (axiosError) {
+        console.error('Error in axios get:', axiosError);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 })
 
 app.get('/photos/:id', async (req, res) => {
-    const { data } = await axios.get(`https://jsonplaceholder.typicode.com/photos/${req.params.id}`)
+    const albumId = req.query.albumId;
+    
+    try {
+        const { data } = await axios.get(
+            `https://jsonplaceholder.typicode.com/photos`,
+            { params: { albumId } }
+        );
 
-    res.json(data)
+        // Wrap the Redis operation in a try-catch block
+        try {
+            await redisClient.setEx('photos', DEFAULT_EXPIRATION, JSON.stringify(data));
+        } catch (redisError) {
+            console.error('Error in Redis setEx:', redisError);
+        }
+
+        res.json(data);
+    } catch (axiosError) {
+        console.error('Error in axios get:', axiosError);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 })
 
 app.listen(3000, () => {
